69387b854394e0f59a8d458463d33feda822e12d
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-09-20 12:50:08 +0200

    adding random number generator that follows the normal distribution

---
+++ random_number_normal_gen.py
@@ -0,0 +1,18 @@
+import sys
+import random
+import numpy
+
+if len(sys.argv) == 2 and sys.argv[1] == "help":
+    print("Usage: python random_number_normal_gen.py [number of random numbers desired] [Mean] [Standard deviation]")
+    sys.exit(0)
+
+numberCount = int(sys.argv[1])
+mean = int(sys.argv[2])
+sd = int(sys.argv[3])
+
+for x in range(numberCount):
+    randomNumber = numpy.random.normal(mean, sd)
+    if (x != numberCount-1):
+        print str(randomNumber)+",",
+    else:
+        print str(randomNumber),

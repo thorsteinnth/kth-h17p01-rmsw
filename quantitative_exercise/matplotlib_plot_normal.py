915f7ee013491cd157a06adf55abe193ca7e66ab
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-09-20 17:08:59 +0200

    adding title and axes labels to plots

--- quantitative_exercise/matplotlib_plot_normal.py
+++ quantitative_exercise/matplotlib_plot_normal.py
@@ -4,11 +4,14 @@ import scipy.stats as stats
 import pylab as pl
 import csv
 
-if len(sys.argv) != 2:
-    print("Usage: python matplotlib_plot_normal.py [filepath]")
+if len(sys.argv) != 5:
+    print("Usage: python matplotlib_plot_normal.py [filepath] [title] [x axis label] [y axis label]")
     sys.exit(0)
 
 filePath = str(sys.argv[1])
+title = str(sys.argv[2])
+xLabel = str(sys.argv[3])
+yLabel = str(sys.argv[4])
 
 dataPoints = []
 
@@ -24,6 +27,9 @@ h = sorted(dataPoints)  #sorted
 fit = stats.norm.pdf(h, np.mean(h), np.std(h))  #this is a fitting indeed
 
 pl.plot(h,fit,'-o')
+pl.title(title)
+pl.xlabel(xLabel)
+pl.ylabel(yLabel)
 
 pl.hist(h,normed=True)      #use this to draw histogram of your data
 

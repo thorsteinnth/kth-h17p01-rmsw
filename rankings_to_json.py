51f84188353541bcfd78f65e7c03caa9c263245d
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-09-27 21:41:46 +0200

    rankings to json - allowing you to specify max number of lines in an output file, splitting the input file into multiple output files

--- rankings_to_json.py
+++ rankings_to_json.py
@@ -4,37 +4,90 @@ import os
 # The data is of the form: pageUrl,pageRank,avgDuration
 # Example: jdprdgnhwrygvizhwxttnprtftlqpncrssviaphxiqmjpkizjwdgxi,6040,8
 
-if len(sys.argv) != 4:
-    print("Usage: python XXX.py [inputfilepath] [outputfilepath] [use couch import format (true/false)]")
+if len(sys.argv) != 5:
+    print("Usage: python XXX.py [inputfilepath] [outputfilepath] [maxlines (0 to turn off)] [use couch import format (true/false)]")
     sys.exit(0)
 
+# Argument parsing
+
 inputFilePath = str(sys.argv[1])
-inputFile = open(inputFilePath, "r")
-
 outputFilePath = str(sys.argv[2])
-outputFile = open(outputFilePath, "w")
+maxLinesPerFile = int(sys.argv[3])
 
-useCouchImportFormat = str(sys.argv[3]) == "true"
+limitNumberOfLines = False
+if (maxLinesPerFile > 0):
+	limitNumberOfLines = True
+
+useCouchImportFormat = str(sys.argv[4]) == "true"
 
 if (useCouchImportFormat):
 	print "Using CouchDB import format"
+	
+if (limitNumberOfLines and maxLinesPerFile < 10000):
+	print "You really shouldn't have max lines per file less than 10000 or something. You will create way to many files and freeze your system. Exiting ..."
+	sys.exit(1)
 
-if (useCouchImportFormat):
-	outputFile.write("{\n\"docs\": [\n")
+if (limitNumberOfLines):
+	print str("Limiting number of lines per output file to " + str(maxLinesPerFile))
+	print str("Output files will be called " + outputFilePath + "_XXX")
+
+# Methods
+
+def getOutputFileName(path, suffix):
+	if (suffix != ""):
+		return path + "_" + suffix
+	else:
+		return path
+
+def openOutputFile(path, suffix):
+	outputFile = open(getOutputFileName(path, suffix), "w")
+	# Couch DB needs output docs to contain a single JSON document
+	if (useCouchImportFormat):
+		outputFile.write("{\n\"docs\": [\n")
+	return outputFile
+
+def closeOutputFile(file):
+	if (useCouchImportFormat):
+		# Couch DB needs output docs to contain a single JSON document
+		# Need to remove the trailing comma, lets just remove the last two characters
+		file.seek(-2, os.SEEK_END)
+		file.truncate()
+		file.write("\n]\n}")
+	file.close()
+
+# Processing
+
+inputFile = open(inputFilePath, "r")
+
+lineCount = 0
+outputFileCount = 0
+
+outputFile = openOutputFile(outputFilePath, str(outputFileCount) if limitNumberOfLines else "")
 
 for line in inputFile:
+
+	if (limitNumberOfLines and lineCount == maxLinesPerFile):
+		# Close the current file and open a new one
+		lineCount = 0
+		outputFileCount = outputFileCount + 1
+		closeOutputFile(outputFile)		
+		outputFile = openOutputFile(outputFilePath, str(outputFileCount))
+
 	# Use rstrip() to remove the newline
 	splitLine = line.rstrip().split(",")
 	formattedOutput = "{{ \"pageUrl\":\"{}\", \"pageRank\":\"{}\", \"avgDuration\":\"{}\" }}".format(splitLine[0], splitLine[1], splitLine[2])
+	
 	if (useCouchImportFormat):
 		outputFile.write(str(formattedOutput) + ",\n")
 	else:
 		outputFile.write(str(formattedOutput) + "\n")
+	
+	lineCount = lineCount+1
 
-if (useCouchImportFormat):
-	# Need to remove the trailing comma, lets just remove the last two characters
-	outputFile.seek(-2, os.SEEK_END)
-	outputFile.truncate()
-	outputFile.write("\n]\n}")
+closeOutputFile(outputFile)
 
-outputFile.close()
+
+
+
+
+

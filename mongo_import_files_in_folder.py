27ef30ea8b80f58a02ea7aa0b734eb67a7c69ef6
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-10-03 16:26:39 +0200

    ignoring .DS_Store files in mongo import

--- mongo_import_files_in_folder.py
+++ mongo_import_files_in_folder.py
@@ -33,12 +33,13 @@ if dropCollection:
 	call(["mongo", "localhost:" + port + "/" + dbname, "--eval", "db.getCollectionNames()"])
 
 # Get the path to all the files in the directory
-files = [str(folderPath + "/" + f) for f in listdir(folderPath) if isfile(join(folderPath, f))]
+files = [str(folderPath + "/" + f) for f in listdir(folderPath) if isfile(join(folderPath, f)) and f != ".DS_Store"]
 
 # Run the import command for all the files
 # mongoimport --db some-db --collection rankings --drop --port 32768 --file /Users/tts/Google\ Drive/School/MSc/KTH/H17P01/Research\ Methodology\ and\ Scientific\ Writing/Project/data/mongo_local/rankings/rankings_json_mongo_0
 for file in files:
 	# NOTE: Can add parameter "--drop" here to to drop the collection before each import
+	print("Importing file: " + file)
 	call(["mongoimport", "--db", dbname, "--collection", collection, "--port", port, "--file", file])
 
 # Print total number of items in collection

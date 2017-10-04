fa9937b3e6d08e609fb8562680e36d47b7642799
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-10-04 11:38:00 +0200

    timing in mongo bench

--- benchmarks/mongodb/local/mongo_bench_local.py
+++ benchmarks/mongodb/local/mongo_bench_local.py
@@ -1,14 +1,21 @@
 import pymongo
 import pprint 
+import timeit
 from pymongo import MongoClient
 from pprint import pprint
+from timeit import Timer
+from functools import partial
+
+def rangeQuery(rankings):
+	# SELECT pageURL, pageRank FROM rankings WHERE pageRank > X
+	# The cursor that find() returns is probably lazy loaded, use count() to force load
+	rankings.find({ "pageRank" : { "$gt": "26" }}).count()
 
 client = MongoClient('localhost', 32768)
 db = client['some-db']
 
 rankings = db.rankings
 
-# SELECT pageURL, pageRank FROM rankings WHERE pageRank > X
-cursor = rankings.find({ "pageRank" : { "$gt": "26" }})
-
-pprint(cursor.count())
+rangeResult = Timer(partial(rangeQuery, rankings)).repeat(10, 1)
+print("Range query result:")
+pprint(rangeResult)

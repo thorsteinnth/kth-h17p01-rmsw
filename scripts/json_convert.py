9ed68d7b462a69972970b6b594b637fd85aa72b9
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-10-12 15:51:37 +0200

    dont treat numbers as strings when formatting data

--- scripts/json_convert.py
+++ scripts/json_convert.py
@@ -85,9 +85,9 @@ def formatLine(line):
 	# Use rstrip() to remove the newline
 	splitLine = line.rstrip().split(",")
 	if (dataformat == "rankings"):
-		return "{{ \"pageUrl\":\"{}\", \"pageRank\":\"{}\", \"avgDuration\":\"{}\" }}".format(splitLine[0], splitLine[1], splitLine[2])
+		return "{{ \"pageUrl\":\"{}\", \"pageRank\":{}, \"avgDuration\":{} }}".format(splitLine[0], splitLine[1], splitLine[2])
 	elif (dataformat == "uservisits"):
-		return "{{ \"sourceIP\":\"{}\", \"destUrl\":\"{}\", \"visitDate\":\"{}\", \"adRevenue\":\"{}\", \"userAgent\":\"{}\", \"countryCode\":\"{}\", \"languageCode\":\"{}\", \"searchWord\":\"{}\", \"duration\":\"{}\" }}".format(splitLine[0], splitLine[1], splitLine[2], splitLine[3], splitLine[4], splitLine[5], splitLine[6], splitLine[7], splitLine[8])
+		return "{{ \"sourceIP\":\"{}\", \"destUrl\":\"{}\", \"visitDate\":\"{}\", \"adRevenue\":{}, \"userAgent\":\"{}\", \"countryCode\":\"{}\", \"languageCode\":\"{}\", \"searchWord\":\"{}\", \"duration\":{} }}".format(splitLine[0], splitLine[1], splitLine[2], splitLine[3], splitLine[4], splitLine[5], splitLine[6], splitLine[7], splitLine[8])
 	else:
 		print("Unknown data format: " + dataformat)
 		sys.exit(1)

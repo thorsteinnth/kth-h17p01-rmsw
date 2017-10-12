478e177e7952c758b67d81dabe05a05e42cf7fca
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-10-12 15:55:52 +0200

    mongo mapreduce benchmark

--- benchmarks/mongodb/local/mongo_bench_local.py
+++ benchmarks/mongodb/local/mongo_bench_local.py
@@ -5,11 +5,22 @@ from pymongo import MongoClient
 from pprint import pprint
 from timeit import Timer
 from functools import partial
+from bson.code import Code
 
 def rangeQuery(rankings):
 	# SELECT pageURL, pageRank FROM rankings WHERE pageRank > X
 	# The cursor that find() returns is probably lazy loaded, use count() to force load
 	rankings.find({ "pageRank" : { "$gt": "26" }}).count()
+
+def mapReduceTotalDurationPerDateQuery(uservisits):
+	# Find the total duration of all visits on each date
+	mapper = Code("""
+		function() { emit( this.visitDate, this.duration ); }
+		""")
+	reducer = Code("""
+		function(key, values) { return Array.sum( values ) }
+		""")
+	uservisits.map_reduce(mapper, reducer, "total_duration_per_date", True)
 
 # Hardcoded authentication stuff
 username = "jsmith"
@@ -24,7 +35,14 @@ print "Authenticated: " + str(authenticated)
 db = client['some-db']
 
 rankings = db.rankings
+uservisits = db.uservisits
 
+print("Running range query...")
 rangeResult = Timer(partial(rangeQuery, rankings)).repeat(10, 1)
 print("Range query result:")
 pprint(rangeResult)
+
+print("Running MapReduce query...")
+mapReduceResult = Timer(partial(mapReduceTotalDurationPerDateQuery, uservisits)).repeat(1, 1)
+print("MapReduce query result:")
+pprint(mapReduceResult)

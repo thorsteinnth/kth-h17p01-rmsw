4818329b7c444b04d5f6f4f82a16f65bd575e142
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-10-17 16:44:12 +0200

    format part numbers in the S3 getter script

--- scripts/s3_getter.py
+++ scripts/s3_getter.py
@@ -23,6 +23,21 @@ partnumber_from = sys.argv[2]
 partnumber_to = sys.argv[3]
 local_save_path = sys.argv[4]
 
+def format_part_number(input):	
+	# The filenames are of the form part-00001
+	# 5 characters, prepended with zeros
+	if (len(input) == 5):
+		return input
+	elif (len(input) > 5):
+		print("Part number should be 5 characters at most")
+		sys.exit(0)
+	else:
+		# Need to prepend zeros
+		prepend_length = 5-len(input)
+		return "0" * prepend_length + input
+
+print("Will get part numbers " + format_part_number(str(partnumber_from)) + " to " + format_part_number(str(partnumber_to)))
+
 for partnumber in range(int(partnumber_from), int(partnumber_to) + 1):
-	print("Getting part number " + str(partnumber) + " ...")
-	call(["s3cmd", "get", "s3://big-data-benchmark/pavlo/text/5nodes/"+ data_type + "/part-000" + str(partnumber), local_save_path])
+	print("Getting part number " + format_part_number(str(partnumber)) + " ...")
+	call(["s3cmd", "get", "s3://big-data-benchmark/pavlo/text/5nodes/"+ data_type + "/part-" + format_part_number(str(partnumber)), local_save_path])
